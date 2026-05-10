781eb31fc766423b477d1ac89634a3ae6b3723a2
Aditya Garemella <aditya.sandilya@gmail.com>
2026-05-11 00:30:15 +0530

    intro to search agents

--- main.py
+++ main.py
@@ -3,32 +3,40 @@ from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 
 load_dotenv()
+from langchain.agents import create_agent
+from langchain.tools import tool
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+from tavily import TavilyClient
+#from langchain_tavily import TavilySearch
 
+
+tavily = TavilyClient()
+
+@tool
+def search(query: str) -> str:
+    """
+    Tool that searches over internet
+    Args:
+        query: The query to search for
+    Returns:
+        The search result
+    """
+#    print(f"Searching for {query}")
+#    #return "Tokyo weather is sunny"
+#    return tavily.search(query=query)
+
+llm = ChatOpenAI(model="gpt-5")
+tools = [search]
+#tools = [TavilySearch()]
+agent = create_agent(model=llm, tools=tools)
 
 def main():
     print("Hello from agentic-ai-project!")
-    information = """
-    Elon Reeve Musk is a businessman and public official known for his leadership of Tesla, SpaceX, X, and xAI. Musk has been the wealthiest person in the world since 2025; as of May 2026, Forbes estimates his net worth to be US$788 billion.
-
-    Born into the wealthy Musk family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002
-    """
-    summary_template = """
-    given the information {information} about a person I want you to create:
-    1. A short summary
-    2. two interesting facts about them
-    """
-
-    summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
-    )
-
-    llm = ChatOpenAI(temperature=0, model="gpt-5")
-    #llm = ChatOllama(temperature=0, model="gemma3:270m")
-    chain = summary_prompt_template | llm
-
-    response = chain.invoke(input={"information": information})
-    print(response.content)
-
+    #result = agent.invoke({"messages":HumanMessage(content="What is the weather in Tokyo?")})
+    result = agent.invoke({"messages":HumanMessage(content="search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details")})
+    print(result)
+    
 
 if __name__ == "__main__":
     main()
