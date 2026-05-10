b3add9d2ccbe8f008da994314a922fcfed1c0bd5
Aditya Garemella <aditya.sandilya@gmail.com>
2026-05-10 20:43:11 +0530

    hello world chain

---
+++ main.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+
+load_dotenv()
+
+
+def main():
+    print("Hello from agentic-ai-project!")
+    information = """
+    Elon Reeve Musk is a businessman and public official known for his leadership of Tesla, SpaceX, X, and xAI. Musk has been the wealthiest person in the world since 2025; as of May 2026, Forbes estimates his net worth to be US$788 billion.
+
+    Born into the wealthy Musk family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002
+    """
+    summary_template = """
+    given the information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model="gpt-5")
+    #llm = ChatOllama(temperature=0, model="gemma3:270m")
+    chain = summary_prompt_template | llm
+
+    response = chain.invoke(input={"information": information})
+    print(response.content)
+
+
+if __name__ == "__main__":
+    main()
